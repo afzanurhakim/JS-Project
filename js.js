cc187ea9d8c258410d43dbe45e1623eb631dae7a
afzanurhakim <73456454+afzanurhakim@users.noreply.github.com>
2020-11-02 17:00:52 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+function myFunction() { 
+        var element = document.body; 
+        element.classList.toggle("dark"); 
+        } 
+		
+		<!-- ===== script for sidebar -->
+function openNav() {
+  document.getElementById("mySidebar").style.width = "175px";
+  document.getElementById("main").style.marginLeft = "175px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+<!-- ===== -->
